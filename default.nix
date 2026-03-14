23e276ae9335404fedf5544b6e75bbd43868cab4
OnlyLinux71 <122188198+OnlyLinux71@users.noreply.github.com>
2026-03-13 23:19:21 -0400

    Refactor default.nix to use pkgs for dependencies

--- default.nix
+++ default.nix
@@ -1,16 +1,19 @@
-{ stdenv, fetchurl, pkg-config, automake, autoconf, libtool, intltool, gtk2 }:
+{ pkgs ? import <nixpkgs> {} }:
 
-stdenv.mkDerivation {
+pkgs.stdenv.mkDerivation {
   pname = "leafpad";
   version = "0.8.17";
 
-  src = fetchurl {
+  src = pkgs.fetchurl {
     url = "https://download-mirror.savannah.gnu.org/releases/leafpad/leafpad-0.8.17.tar.gz";
     sha256 = "jfjeeq6iYUgiW2EgYxtP5ridNtK1KWLnycwM4Hv9vUw="; # working hash
   };
 
-  nativeBuildInputs = [ pkg-config automake autoconf libtool intltool ];
-  buildInputs = [ gtk2 ];
+  nativeBuildInputs = [
+    pkgs.pkg-config pkgs.automake pkgs.autoconf pkgs.libtool pkgs.intltool
+  ];
+
+  buildInputs = [ pkgs.gtk2 ];
 
   configurePhase = ''
     ./autogen.sh || autoreconf -vi
@@ -20,7 +23,7 @@ stdenv.mkDerivation {
   installPhase = "make install";
 
   meta = {
-    description = "Simple GTK text editor (Leafpad 0.8.17)";
-    license = stdenv.lib.licenses.gpl2;
+    description = "Simple GTK text editor (from Savannah mirror)";
+    license = pkgs.lib.licenses.gpl2;
   };
 }
