c1f803eeb7301c62c37a4de7d766ee7f83f1f588
OnlyLinux71 <122188198+OnlyLinux71@users.noreply.github.com>
2026-03-14 08:14:17 -0400

    Refactor default.nix for readability and GCC fix
    Reformatted the default.nix file for better readability and added a fix for GCC warnings.

--- default.nix
+++ default.nix
@@ -6,21 +6,36 @@ pkgs.stdenv.mkDerivation {
 
   src = pkgs.fetchurl {
     url = "https://download-mirror.savannah.gnu.org/releases/leafpad/leafpad-0.8.17.tar.gz";
-    sha256 = "jfjeeq6iYUgiW2EgYxtP5ridNtK1KWLnycwM4Hv9vUw="; # working hash
+    sha256 = "jfjeeq6iYUgiW2EgYxtP5ridNtK1KWLnycwM4Hv9vUw=";
   };
 
   nativeBuildInputs = [
-    pkgs.pkg-config pkgs.automake pkgs.autoconf pkgs.libtool pkgs.intltool
+    pkgs.pkg-config
+    pkgs.automake
+    pkgs.autoconf
+    pkgs.libtool
+    pkgs.intltool
   ];
 
-  buildInputs = [ pkgs.gtk2 ];
+  buildInputs = [
+    pkgs.gtk2
+  ];
+
+  # Fix for modern GCC treating format-security warnings as errors
+  NIX_CFLAGS_COMPILE = "-Wno-error=format-security";
 
   configurePhase = ''
     ./autogen.sh || autoreconf -vi
     ./configure --prefix=$out
   '';
-  buildPhase = "make";
-  installPhase = "make install";
+
+  buildPhase = ''
+    make
+  '';
+
+  installPhase = ''
+    make install
+  '';
 
   meta = {
     description = "Simple GTK text editor (from Savannah mirror)";
